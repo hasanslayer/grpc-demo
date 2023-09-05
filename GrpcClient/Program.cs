0c207c9adb5e7292e7b7d9f3c1c7e38cbbf15346
hasanslayer <hasan-dr2010@hotmail.com>
2023-09-05 10:25:44 +0300

    Add project files.

---
+++ GrpcClient/Program.cs
@@ -0,0 +1,26 @@
+﻿using Grpc.Net.Client;
+using GrpcServer;
+using System;
+
+namespace GrpcClient // Note: actual namespace depends on the project name.
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            // gRPC Client could be any type of project: console, web, api ...
+
+            var input = new HelloRequest() { Name = "Hasan" };
+
+
+            var channel = GrpcChannel.ForAddress("https://localhost:7187");
+            var client = new Greeter.GreeterClient(channel);
+
+            var reply = await client.SayHelloAsync(input);
+
+            Console.WriteLine(reply.Message);
+
+            Console.ReadKey();
+        }
+    }
+}
